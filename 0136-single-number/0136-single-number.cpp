bc9086ea5ccff72d78147977f5a0c11939c55744
hari <96715146+hariiiiiiiii@users.noreply.github.com>
2026-04-05 22:02:57 +0530

    0136-SINGLE-NUMBER Stats: Time: 0 ms (100%), Space: 20.6 MB (58.14%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i = 0; i < nums.size() - 1;i+=2){
-            if(nums[i] != nums[i + 1]){
-                return nums[i];
-            }
-            
+        int result = 0;
+        for(int num : nums){
+            result ^= num;
         }
-        return nums.back();
+        return result;
     }
 };
